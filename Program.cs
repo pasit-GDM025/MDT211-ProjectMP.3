68085fd96c86e59cd4dddc2381d113b799f706fb
pasit-GDM025 <pasit.pipat@mail.kmutt.ac.th>
2022-11-26 20:07:20 +0700

    YOUDEAD

--- Program.cs
+++ Program.cs
@@ -9,16 +9,19 @@ enum LoginMenu{
 }
 class Program {
     public static char starter; //ในตัวเต็มจะรับ ค่าจากการ login
-    static void Main()
+    public static void Main()
     {
-        starting(); 
+        
+    }
+    public static void WhenStart(){
+         starting(); 
         if(starter != '+'){ // เมื่อ login ได้ จะเริ้มทำงานส่วนนี้
             explained();//ส่านนี้จะอธิบายให้ userว่าต้องกดอะไรบ้าง
              inputCommand(); //รับค่าให้ ีuser พิม 1 2 3 แล้วเก็บในลิสด์ 
 
         }
     }
-    static void starting(){
+    public static void starting(){
         char Command;
         Console.Clear();
         Console.WriteLine(":::::::::::::::::::");
@@ -27,23 +30,38 @@ class Program {
         starter = char.Parse(Console.ReadLine());
 
     }
-    static void inputCommand(){
+    public static void inputCommand(){
         char Command;
         if(starter != '+'){
               Console.WriteLine("Please Input play order: ");
 
         Command = char.Parse(Console.ReadLine());
         CircularLinkedList<char> Playlist = new CircularLinkedList<char>();
-        while(Command == '1'|| Command == '2'|| Command == '3'){ // ยังไม่เสด
+        while(Command == '1'|| Command == '2'|| Command == '3')
+        { // ยังไม่เสด
 
         }
-
     }
-
 }
-static void explained(){
+public static void explained(){
     Console.Clear();
     Console.WriteLine("-press number 1 - 3 to making a track- ");
     Console.WriteLine("Then Press 'p' to play");
 }
+public void PrintMenu(){
+    Console.Clear();
+    PrintStartingMenu();
+}
+public void PrintStartingMenu(){
+    Console.Clear();
+    Console.WriteLine(":::::::::::::::::::");
+    Console.WriteLine("Welcome to Mucsic player");
+    Console.WriteLine("1.Log in");
+    Console.WriteLine("2.Track Player");
+    Console.WriteLine(":::::::::::::::::::");
+}
+public static void InputFromKeyboard(){
+    
+}
+
 }
